56c81f4a2e8bc6af9a4eda29fbf23fb824aa8763
Koushik <koushikbethu.indian@gmail.com>
2024-12-13 14:04:59 +0530

    Create ascending_descending_list.py

---
+++ ascending_descending_list.py
@@ -0,0 +1,9 @@
+numbers=[10,2,3,6,5,8,9]
+
+ascending_order=sorted(numbers )#reverse = False
+descending_order=sorted(numbers, reverse = True)
+
+
+print("original List :",numbers)
+print("ascending_order :",ascending_order)
+print("descending_order :",descending_order)
