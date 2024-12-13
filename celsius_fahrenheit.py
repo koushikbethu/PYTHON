27c59907a2de374ba6e6190475bf56e82e840a34
Koushik <koushikbethu.indian@gmail.com>
2024-12-13 12:58:13 +0530

    Create celsius_fahrenheit.py

---
+++ celsius_fahrenheit.py
@@ -0,0 +1,8 @@
+celsius=25.0
+fahrenheit=77.0
+
+result1=(celsius*9/5)+32
+print(f"{celsius}C is equal to the {fahrenheit}F")
+
+result2=(fahrenheit-32)*5/9
+print(f"{fahrenheit}F is equal to the {celsius}C")
