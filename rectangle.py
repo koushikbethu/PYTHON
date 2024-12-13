672b15ff7c2171e23a6f4963cabc0c77986ead3b
Koushik <koushikbethu.indian@gmail.com>
2024-12-13 13:08:25 +0530

    Create rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,9 @@
+length=int(input("Enter the length of the rectangle:"))
+breadth=int(input("Enter the breadth of the rectangle:"))
+
+area=length*breadth
+print(f"the area of the rectangle is {area} square units")
+
+perimeter=2*(length+breadth)
+print(f"the perimeter of the rectangle is {perimeter} units")
+
